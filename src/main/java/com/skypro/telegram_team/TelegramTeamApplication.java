bfff7f7f7970050490aa6cd8475a0d85dfde1927
Sergey Lyubimov <see1rg@mail.ru>
2023-03-23 17:07:40 +0300

    Initial commit

---
+++ src/main/java/com/skypro/telegram_team/TelegramTeamApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.telegram_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramTeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/telegram_team/TelegramTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.telegram_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
