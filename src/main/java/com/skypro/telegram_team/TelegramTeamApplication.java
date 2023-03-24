11ee308558468e89870ca104a9d849d2df07c2c7
11th <11th@bk.ru>
2023-03-24 16:36:22 +0300

    Test commit

--- src/main/java/com/skypro/telegram_team/TelegramTeamApplication.java
+++ src/main/java/com/skypro/telegram_team/TelegramTeamApplication.java
@@ -8,6 +8,6 @@ public class TelegramTeamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TelegramTeamApplication.class, args);
+        System.out.println("Hello!");
     }
-
 }
