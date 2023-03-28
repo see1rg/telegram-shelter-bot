178029067db9c59f06fc6610ee38bebe2c40cd7e
Sergey Lyubimov <see1rg@mail.ru>
2023-03-28 06:35:40 +0300

    commit 2 prepare

--- src/main/java/com/skypro/telegram_team/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/skypro/telegram_team/listener/TelegramBotUpdatesListener.java
@@ -2,14 +2,16 @@ package com.skypro.telegram_team.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
@@ -22,13 +24,26 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public void init() {
         telegramBot.setUpdatesListener(this);
     }
+    //  устанавливает данный класс в качестве слушателя обновлений.
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
-        });
+        // обрабатывает полученные обновления от Telegram Bot API
+        // и вызывает метод accept(Update update) для каждого обновления.
+        try {
+            updates.forEach(update -> {
+                logger.info("Processing update: {}", update);
+                Message message = update.message();
+                Long chatId = message.chat().id();
+                String text = message.text();
+
+                if ("/start".equals(text)){
+                    //service
+                }
+            });
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 }
