8cf6a713968206889e927850dd1dadb4d9bf5172
Dima <akmeev2011@yandex.ru>
2023-03-29 23:58:54 +0300

    Сделал многоуровневую клавиатуру для взаимодействия пользователя с ботом

---
+++ src/main/java/com/skypro/telegram_team/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,105 @@
+package com.skypro.telegram_team.listener;
+
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramException;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Poll;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.*;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class TelegramBotUpdatesListener implements UpdatesListener {
+    Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    @Value("${telegram.bot.name}")
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+            if ("/start".equals(update.message().text())) {
+                Keyboard keyboard = new ReplyKeyboardMarkup
+                        (new KeyboardButton("Узнать информацию о приюте"))
+                        .addRow("Как взять собаку из приюта?")
+                        .addRow("Прислать отчет о питомце")
+                        .addRow("Позвать волонтера")
+                        .oneTimeKeyboard(true);
+                SendMessage sendMessage = new SendMessage(update.message().chat().id(),
+                        "*Привет, я бот, который помогает людям завести питомца и обрести в его лице верного друга*");
+                sendMessage.parseMode(ParseMode.MarkdownV2).replyMarkup(keyboard);
+                SendResponse sendResponse = telegramBot.execute(sendMessage);
+            } else if ("Узнать информацию о приюте".equals(update.message().text())) {
+                Keyboard keyboard = new ReplyKeyboardMarkup(
+                        new KeyboardButton("Записать контактные данные для связи"))
+                        .addRow("Позвать волонтера");
+                SendMessage sendMessage = new SendMessage(update.message().chat().id(),
+                        "*Привет. Какая-то информация о приюте*");
+                sendMessage.parseMode(ParseMode.MarkdownV2).replyMarkup(keyboard);
+                SendResponse sendResponse = telegramBot.execute(sendMessage);
+            } else if ("Записать контактные данные для связи".equals(update.message().text())) {
+                SendMessage sendMessage = new SendMessage(update.message().chat().id(),
+                        "Введите данные о себе в формате: ФИО, номер телефона");
+                SendResponse sendResponse = telegramBot.execute(sendMessage);
+            } else if ("Как взять собаку из приюта?".equals(update.message().text())) {
+                Keyboard keyboard = new ReplyKeyboardMarkup
+                        (new KeyboardButton("Узнать правила знакомства с собакой до того, как можно забрать ее из приюта"))
+                        .addRow("Получить список документов, необходимых для того, чтобы взять собаку из приюта.")
+                        .addRow("Получить список рекомендаций по транспортировке животного")
+                        .addRow("Получить список рекомендаций по обустройству дома для щенка")
+                        .addRow("Получить список рекомендаций по обустройству дома для взрослой собаки")
+                        .addRow("Получить список рекомендаций по обустройству дома для собаки с ограниченными возможностями (зрение, передвижение)")
+                        .addRow("Получить советы кинолога по первичному общению с собакой")
+                        .addRow("Получить рекомендации по проверенным кинологам для дальнейшего обращения к ним")
+                        .addRow("Получить список причин, почему могут отказать и не дать забрать собаку из приюта")
+                        .addRow("Записать контактные данные для связи")
+                        .addRow("Позвать волонтера")
+                        .oneTimeKeyboard(true);
+                SendMessage sendMessage = new SendMessage(update.message().chat().id(),
+                        "");
+                sendMessage.replyMarkup(keyboard).parseMode(ParseMode.MarkdownV2);
+                SendResponse sendResponse = telegramBot.execute(sendMessage);
+            } else if ("Прислать отчет о питомце".equals(update.message().text())) {
+                Keyboard keyboard = new ReplyKeyboardMarkup
+                        (new KeyboardButton("Получить форму ежедневного отчета"))
+                        .addRow("Позвать волонтера")
+                        .oneTimeKeyboard(true);
+                SendMessage sendMessage = new SendMessage(update.message().chat().id(),
+                        "");
+                sendMessage.replyMarkup(keyboard);
+                SendResponse sendResponse = telegramBot.execute(sendMessage);
+            } else if ("Получить форму ежедневного отчета".equals(update.message().text())) {
+                SendMessage sendMessage = new SendMessage(update.message().chat().id(),
+                        "*В ежедневный отчет входит следующая информация:*\n" +
+                                "- *Фото животного.*\n" +
+                                "- *Рацион животного.*\n" +
+                                "- *Общее самочувствие и привыкание к новому месту.*\n" +
+                                "- *Изменение в поведении: отказ от старых привычек, приобретение новых.*");
+                sendMessage.parseMode(ParseMode.MarkdownV2);
+                SendResponse sendResponse = telegramBot.execute(sendMessage);
+            }
+        });
+        return CONFIRMED_UPDATES_ALL;
+    }
+}
+
+
+
