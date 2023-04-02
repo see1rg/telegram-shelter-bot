e42965b50e0f35e97e1317c46828729bd1852516
see1rg <110130152+see1rg@users.noreply.github.com>
2023-04-02 17:14:08 +0300

    add javadoc and api doc

--- src/main/java/com/skypro/telegram_team/controllers/UserController.java
+++ src/main/java/com/skypro/telegram_team/controllers/UserController.java
@@ -2,6 +2,12 @@ package com.skypro.telegram_team.controllers;
 
 import com.skypro.telegram_team.models.User;
 import com.skypro.telegram_team.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,27 +18,57 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+    @Operation(summary = "Поиск пользователя по id", description = "Get user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Поиск пользователя по id", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))
+            })
+    }
+    )
     @GetMapping("/{id}")
-    public User getUser(@PathVariable long id) {
+    public User getUser(@Parameter(description = "Id пользователя") @PathVariable long id) {
         return userService.findById(id);
     }
 
+    @Operation(summary = "Получение списка всех пользователей", description = "Get all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Получение списка всех пользователей", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))
+            })
+    })
     @GetMapping
     public Iterable<User> getAllUsers() {
         return userService.findAll();
     }
 
+    @Operation(summary = "Удаление пользователя по id", description = "Delete user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Удаление пользователя по id", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))
+            })
+    })
     @DeleteMapping("/{id}")
     public User deleteUser(@PathVariable long id) {
         return userService.deleteById(id);
     }
 
+    @Operation(summary = "Создание пользователя", description = "Create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Создание пользователя", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))
+            })
+    })
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.save(user);
     }
 
+    @Operation(summary = "Обновление данных пользователя", description = "Update user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Обновление данных пользователя", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))
+            })
+    })
     @PutMapping("/{id}")
     public User updateUser(@RequestBody User user, @PathVariable Long id) {
         return userService.update(user,id);
