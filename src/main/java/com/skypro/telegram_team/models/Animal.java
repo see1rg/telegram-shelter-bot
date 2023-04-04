d4a9cae96e53e9b8763fdff5e3915fd9a9600b30
see1rg <110130152+see1rg@users.noreply.github.com>
2023-04-04 22:54:35 +0300

    fix

--- src/main/java/com/skypro/telegram_team/models/Animal.java
+++ src/main/java/com/skypro/telegram_team/models/Animal.java
@@ -26,6 +26,7 @@ public class Animal {
     private String description;
     private LocalDateTime startTest;
     private int daysForTest;
+    private long userId;
     @OneToOne
     @JoinColumn(name = "owner_id")
     private User owner;
